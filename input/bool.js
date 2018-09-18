42de1f32022e2d5f07ab4f3f129f6bc1d6fbf972
Dušan Marsa <dusanmarsa99@gmail.com>
2018-09-18 15:13:19 +0200

    - add custom header checkbox to inputs - checkbox is enabled only if "onHeaderCheckedChange" array is provided with name of checkbox text and function to catch change

--- input/bool.js
+++ input/bool.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { observer } from 'mobx-react'
-import { Checkbox } from 'react-bootstrap'
+import {observer} from 'mobx-react'
+import {Checkbox, FormGroup} from 'react-bootstrap'
 
-const BStrapBoolInput = ({attr, label, record, onChange, valuemapping, errors, ...rest}) => {
-
+const BStrapBoolInput = ({
+  attr, label, record, onChange, onHeaderCheckedChange, valuemapping, errors, ...rest
+}) => {
+  //
   const checked = Boolean(record.get(attr))
 
   const handleChange = (event) => {
-    const newVal = ! checked
+    const newVal = !checked
     onChange(attr, valuemapping ? valuemapping[newVal] : newVal)
   }
 
   return (
-    <Checkbox checked={checked} onChange={handleChange} {...rest}>{label}</Checkbox>
+    <FormGroup>
+      <Checkbox inline checked={checked} onChange={handleChange} {...rest}> {label} </Checkbox>
+      { onHeaderCheckedChange && Array.isArray(onHeaderCheckedChange) && 
+        onHeaderCheckedChange[0] && onHeaderCheckedChange[1]
+        ? <Checkbox 
+            inline style={{ marginLeft: '5px' }} 
+            onChange={e => onHeaderCheckedChange[1](attr, e.target.checked)}>
+            {onHeaderCheckedChange[0]}
+          </Checkbox>
+        : null
+      }
+    </FormGroup>
   )
 }
 
